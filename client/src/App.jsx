3b4132249390bf9bbef1a90a0c1741640afb5a48
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-04-09 19:45:14 +0530

    minor changes

---
+++ client/src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import LandingPage from "./Pages/Landingpage";
+import Plasmadonor from "./Pages/Plasmadonor";
+import Hospitalbeds from "./Pages/Hospitalbeds";
+import Oxygensuppliers from "./Pages/Oxygensupply";
+import Medssuppliers from "./Pages/Medssuppliers";
+import Ambulanceprovider from "./Pages/Ambulanceproviders";
+import Plasmadonorcontact from "./Pages/Plasmadonorcontact";
+import Hospitalbedcontact from "./Pages/Hospitalbedcontact";
+import Oxygensupplycontact from "./Pages/Oxygensupplycontact";
+import Medssuppliercontact from "./Pages/Medssuppliercontact";
+import Ambulanceprovidercontact from "./Pages/Ambulanceprovidercontact";
+import Contact from "./Pages/Contact";
+import { useEffect } from "react";
+import Docstate from "./context/Docstate";
+
+function App() {
+    return (
+        <>
+            <Docstate>
+                <Router>
+                    {/* <Navbar /> */}
+                    <Switch>
+                        <Route path="/" exact component={LandingPage} />
+                        <Route path="/plasma" exact component={Plasmadonor} />
+                        <Route path="/hospitalbeds" exact component={Hospitalbeds} />
+                        <Route path="/oxygensupply" exact component={Oxygensuppliers} />
+                        <Route path="/medssupply" exact component={Medssuppliers} />
+                        <Route
+                            path="/ambulanceprovider"
+                            exact
+                            component={Ambulanceprovider}
+                        />
+                        <Route
+                            path="/plasmadonorcontact"
+                            exact
+                            component={Plasmadonorcontact}
+                        />
+                        <Route
+                            path="/hospitalbedcontact"
+                            exact
+                            component={Hospitalbedcontact}
+                        />
+                        <Route
+                            path="/oxygensupplycontact"
+                            exact
+                            component={Oxygensupplycontact}
+                        />
+                        <Route
+                            path="/medssupplycontact"
+                            exact
+                            component={Medssuppliercontact}
+                        />
+                        <Route
+                            path="/ambulanceprovidercontact"
+                            exact
+                            component={Ambulanceprovidercontact}
+                        />
+                        <Route path="/contact" exact component={Contact} />
+                    </Switch>
+                </Router>
+            </Docstate>
+
+        </>
+    );
+}
+
+export default App;
