b25a3a18696df0063f801e0be4eeb31637f68390
Nitin Rajesh <nitinrajesh094@gmail.com>
2022-06-23 21:08:35 +0530

    Added about section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -35,7 +35,10 @@ const Home = () => {
             <img src={headingPhoto} alt="heading" width="600" height="600" />
           </div>
         </div>
-        <div className="about">About</div>
+        <div className="about-container">
+          <h3>About Us</h3>
+          <p>DocTalk is your ultimate medical help space. Here, at DocTalk you will be helped with all the services catering towards the medical field like Plasma donors’ hospital beds oxygen suppliers medicine suppliers and ambulance providers. DocTalk is your one step solution. Just log into your account and book your appointment right away. you can even call us for your appointment, and we will get back to you shortly.</p>
+        </div>
         <div className="services">
           <h4>Services</h4>
           <div className="services-container">
