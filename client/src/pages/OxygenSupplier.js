70b6ee23dbfd28610c9e3cbbfcd4d01af1fe0d92
Sarrene Madi <p15006393@student.newinti.edu.my>
2022-07-11 04:53:05 +0800

    Registration of Oxygen Suppliers

--- client/src/pages/OxygenSupplier.js
+++ client/src/pages/OxygenSupplier.js
@@ -4,17 +4,52 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 import "../styles/OxygenSupplier.css";
+import { BsPersonSquare,BsTelephoneFill,BsGlobe,BsBuilding,BsFillHouseFill } from 'react-icons/bs';
 
-const OxygenSupplier = () => {
+function DonatePlasma() {
   return (
     <>
-      <Navbar />
-      <div className="container">
-        OxygenSuppliers
-      </div>
-      <Footer />
+        <Navbar/>
+        <div className='oxygen-supplier'>
+            <h2 className='heading'>Oxygen Supplier Registration</h2>
+            <form className='oxygen-supplier-form'>
+                <div className="input-field">
+                    <div className="input-image">
+                    <BsPersonSquare size={60} color="#0603E6"/>
+                    </div>
+                    <input type="text" placeholder='Name'/>
+                </div>
+                <div className="input-field">
+                    <div className="input-image">
+                    <BsTelephoneFill size={60} color="#0603E6"/>
+                    </div>
+                    <input type="text" placeholder='Phone'/>
+                </div>
+                <div className="input-field">
+                    <div className="input-image">
+                    <BsFillHouseFill size={60} color="#0603E6"/>
+                    </div>
+                    <input type="text" placeholder='Address'/>
+                </div>
+                <div className="input-field">
+                    <div className="input-image">
+                    <BsGlobe size={60} color="#0603E6"/>
+                    </div>
+                    <input type="text" placeholder='Country'/>
+                </div>
+                <div className="input-field">
+                    <div className="input-image">
+                    <BsBuilding size={60} color="#0603E6"/>
+                    </div>
+                    <input type="text" placeholder='City'/>
+                </div>
+                <div className="btn-field">
+                    <button>Register</button>
+                </div>
+            </form>
+        </div>
     </>
   )
 }
 
-export default OxygenSupplier
+export default DonatePlasma
