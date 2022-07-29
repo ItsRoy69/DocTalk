7c7e6bad0ff4c4fa33fbd6002ad09cb1ca867f28
rohit <rohitdevops.roy@gmail.com>
2022-07-29 14:29:21 +0530

    fix: Updated AboutUs Page !

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -43,8 +43,18 @@ const Home = () => {
             <img src={headingPhoto} alt="heading" width="600" height="600" />
           </div>
         </div>
-        <div className="about" id="about">
-          About
+        <div className="aboutus" id="about">
+        <h4>About Us</h4>
+        <div className="aboutus-container"> 
+          <p className="aboutus-text"> 
+          DocTalk is your ultimate medical help space. Here, at DocTalk you will be helped with all the 
+          services catering towards the medical field like Plasma donors' hospital beds oxygen suppliers 
+          medicine suppliers and ambulance providers. DocTalk is your one step solution. Just log into 
+          your account and book your appointment right away. you can even call us for your appointment, 
+          and we will get back to you shortly.
+          </p>
+        </div>
+
         </div>
         <div className="services">
           <h4>Services</h4>
