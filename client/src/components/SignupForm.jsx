5b4aaa566f53e4479faf679b2341981ec4f940de
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-08-28 12:22:58 +0600

    Create SignupForm.jsx

---
+++ client/src/components/SignupForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Signup.css"
+
+export default function SignupForm(){
+    return(
+        <div className="signup-container">
+            <div className="signup-form">
+                <h1>Welcome to DocTalk</h1>
+                <h3>Signup as a</h3>
+                <div className="user-types">
+                    <Link to="doctor"><div className="user-type">Doctor</div></Link>
+                    <Link to="donor"><div className="user-type">Donor</div></Link>
+                    <Link to="patient"><div className="user-type">Patient</div></Link>
+                </div>
+            </div>
+        </div>
+    )
+};
