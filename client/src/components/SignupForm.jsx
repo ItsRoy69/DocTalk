e41d0aebff4a2f769f7503db39cc22c9021e36b9
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-08-28 13:38:32 +0600

    Update SignupForm.jsx

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -9,9 +9,9 @@ export default function SignupForm(){
                 <h1>Welcome to DocTalk</h1>
                 <h3>Signup as a</h3>
                 <div className="user-types">
-                    <Link to="doctor"><div className="user-type">Doctor</div></Link>
-                    <Link to="donor"><div className="user-type">Donor</div></Link>
-                    <Link to="patient"><div className="user-type">Patient</div></Link>
+                    <Link to="/doctorsregistration"><div className="user-type">Doctor</div></Link>
+                    <Link to="/donateplasma"><div className="user-type">Donor</div></Link>
+                    <Link to="/patientsregistration"><div className="user-type">Patient</div></Link>
                 </div>
             </div>
         </div>
