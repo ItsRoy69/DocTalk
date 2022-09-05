b52cd334992e2d56722f85995cb59bc74b37be72
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-09-05 08:57:41 +0530

    debug

---
+++ client/src/pages/OxygenSupplier.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+import "../styles/OxygenSupplier.css";
+
+
+function DonatePlasma() {
+  return (
+    <>
+        <Navbar/>
+        List of Oxygen Suppliers
+    </>
+  )
+}
+
+export default DonatePlasma
