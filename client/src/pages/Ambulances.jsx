b2166a59345795b656337f59b2de19eef2b8e000
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-09-05 09:00:14 +0530

    de4bug

--- client/src/pages/Ambulances.jsx
+++ client/src/pages/Ambulances.jsx
@@ -1,3 +1,5 @@
+// Ambulance Page
+
 import Navbar from "../components/Navbar"
 import Ambulance from "../components/Ambulance"
 import Footer from "../components/Footer"
@@ -58,4 +60,4 @@ const Ambulances = () => {
 	)
 }
 
-export default Ambulances
+export default Ambulances;
