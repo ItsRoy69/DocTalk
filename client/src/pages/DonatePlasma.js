51b90fbd953345ebcae21c8aa98e4a51251255b3
Vansh Baghel <103327712+Vansh-Baghel@users.noreply.github.com>
2022-09-07 12:05:42 -0700

    Made 8 changes
    1. Added phone number logic for valid number.
    2. Made all the fields compulsory.
    3. Fixed the placeholder texts.
    4. Added rest of the icons

--- client/src/pages/DonatePlasma.js
+++ client/src/pages/DonatePlasma.js
@@ -2,7 +2,8 @@ import React,{ useState, useEffect} from "react";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import "../styles/DonatePlasma.css";
-import { BsPersonSquare,BsTelephoneFill,BsGlobe,BsBuilding,BsFillHouseFill,BsDropletHalf,BsEnvelopeFill } from 'react-icons/bs';
+import { BsPersonSquare,BsTelephoneFill,BsGlobe,BsBuilding,BsFillHouseFill,BsDropletHalf, BsCalendarDate } from 'react-icons/bs';
+import {MdOutlinePersonPinCircle} from 'react-icons/md'
 
 function DonatePlasma() {
 
@@ -15,7 +16,8 @@ function DonatePlasma() {
     const [city, setCity] = useState();
     const [pincode, setPincode] = useState();
 
-    function handleSubmit(){
+    function handleSubmit(e){
+        e.preventDefault();
         const request = {
             method:'POST',
             mode:'cors',
@@ -35,6 +37,15 @@ function DonatePlasma() {
         }).catch(function (e) {
             console.log(e);
         });
+
+        // Condition for appropriate mobile number
+        let NumberValidation = document.getElementById("numberInput").value;
+        if (NumberValidation.length < 10 || NumberValidation < 1){
+            alert("Not a Valid Number");
+        }
+        else{
+            alert("Correct Number");
+        }
     };
 
     useEffect(()=>{
@@ -50,49 +61,56 @@ function DonatePlasma() {
     },[name,phone,bloodGroup,cDate,address,city,pincode]);
 
     return (
-    <>
+        <>
         <Navbar/>
         <div className='donate-plasma'>
             <h2 className='heading'>Add Yourself as Plasma Donor</h2>
-            <form onSubmit={(e)=>{handleSubmit(); e.preventDefault();}} className='donate-plasma-form'>
+            <form onSubmit={handleSubmit} className='donate-plasma-form'>
                 <div className="input-field">
                     <div className="input-image">
-                    <BsPersonSquare size={60} color="#0603E6"/>
+                    <BsPersonSquare size={40} color="#0603E6"/>
                     </div>
-                    <input value={name} type="text" onChange={(e)=>{setName(e.target.value)}} placeholder='Name'/>
-                </div>
-                <div className="input-field">
-                    <div className="input-image">
-                    <BsTelephoneFill size={60} color="#0603E6"/>
-                    </div>
-                    <input value={phone} type="text" onChange={(e)=>{setPhone(e.target.value)}} placeholder='Phone'/>
+                    <input value={name} type="text" onChange={(e)=>{setName(e.target.value)}} placeholder='Name' required/>
                 </div>
                 <div className="input-field">
                     <div className="input-image">
-                    <BsBuilding size={60} color="#0603E6"/>
+                    <BsTelephoneFill size={40} color="#0603E6"/>
                     </div>
-                    <input value={city} type="text" onChange={(e)=>{setCity(e.target.value)}} placeholder='City'/>
+                    <input value={phone} type="number" onChange={(e)=>{setPhone(e.target.value)}} placeholder='Phone' id="numberInput" required/>
                 </div>
                 <div className="input-field">
                     <div className="input-image">
-                    <BsFillHouseFill size={60} color="#0603E6"/>
+                    <BsBuilding size={40} color="#0603E6"/>
                     </div>
-                    <input value={address} type="text" onChange={(e)=>{setAddress(e.target.value)}} placeholder='Address'/>
+                    <input value={city} type="text" onChange={(e)=>{setCity(e.target.value)}} placeholder='City' required/>
                 </div>
                 <div className="input-field">
                     <div className="input-image">
-                    <BsDropletHalf size={60} color="#0603E6"/>
+                    <BsFillHouseFill size={40} color="#0603E6"/>
+                    
                     </div>
-                    <input value={bloodGroup} type="text" onChange={(e)=>{setBloodGroup(e.target.value)}} placeholder='Blood Group'/>
+                    <input value={address} type="text" onChange={(e)=>{setAddress(e.target.value)}} placeholder='Address' required/>
                 </div>
                 <div className="input-field">
-                    <input value={cDate} type="text" onChange={(e)=>{setCDate(e.target.value)}} placeholder='C_Date'/>
+                    <div className="input-image">
+                    <BsDropletHalf size={40} color="#0603E6"/>
+                    </div>
+                    <input value={bloodGroup} type="text" onChange={(e)=>{setBloodGroup(e.target.value)}} placeholder='Blood Group' required/>
                 </div>
                 <div className="input-field">
-                    <input value={pincode} type="text" onChange={(e)=>{setPincode(e.target.value)}} placeholder='Pincode'/>
+                <div className="input-image">
+                    <BsCalendarDate size={40} color="#0603E6"/>
+                    </div>
+                    <input value={cDate} type="text" onChange={(e)=>{setCDate(e.target.value)}}  placeholder='Date' required/>
+                </div>
+                <div className="input-field">
+                <div className="input-image">
+                    <MdOutlinePersonPinCircle size={40} color="#0603E6"/>
+                    </div>
+                    <input value={pincode} type="text" onChange={(e)=>{setPincode(e.target.value)}}  placeholder='Pincode' required/>
                 </div>
                 <div className="btn-field">
-                    <button>Register</button>
+                    <button type="submit">Register</button>
                 </div>
             </form>
         </div>
