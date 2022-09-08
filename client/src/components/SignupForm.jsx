5a18693ee9b4335d9045eb2413ee058b4124fecd
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-09-08 23:14:47 +0600

    Update SignupForm.jsx

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/Signup.css"
+import "../styles/Signup.css";
+import doctor from '../assets/images/doctor1.png';
+import donor from '../assets/images/donor.png';
+import patient from '../assets/images/patient.png';
 
 export default function SignupForm(){
     return(
         <div className="signup-container">
             <div className="signup-form">
-                <h1>Welcome to DocTalk</h1>
-                <h3>Signup as a</h3>
+                <p className="welcome-text">Welcome to DocTalk</p>
+                <p className="signup-text">Signup as a</p>
                 <div className="user-types">
-                    <Link to="/doctorsregistration"><div className="user-type">Doctor</div></Link>
-                    <Link to="/donateplasma"><div className="user-type">Donor</div></Link>
-                    <Link to="/patientsregistration"><div className="user-type">Patient</div></Link>
+                    <Link to="/doctorsregistration"><div className="user-type"><img alt="doctor" src={doctor}/>Doctor</div></Link>
+                    <Link to="/donateplasma"><div className="user-type"><img alt="donor" src={donor}/>Donor</div></Link>
+                    <Link to="/patientsregistration"><div className="user-type"><img alt="patient" src={patient}/>Patient</div></Link>
                 </div>
+                <Link to="/signin"><p className="signin-route">Already have an account? Click here.</p></Link>
             </div>
         </div>
     )
