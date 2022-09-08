d9887f889e6383fdf7900d6a349221cb981832fc
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-09-08 23:29:13 +0600

    Update Plasma.js

--- server/routes/Plasma.js
+++ server/routes/Plasma.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const Plasma = require("../models/Plasma");
+const router = require("express").Router();
+const {Plasma} = require("../models/Plasma");
+const { hashPassword,comparePassword } = require("../utils/bcrypt");
 
 //* Route 1
 router.get("/plasma", async (req, res) => {
@@ -13,24 +13,85 @@ router.get("/plasma", async (req, res) => {
 });
 
 //* Route 2
+//Registering a donor
+
 router.post("/plasma", async (req, res) => {
-  try {
-    const { name, phone, bloodGroup, cDate, address, city, pincode } =
-      req.body.formData;
-    const plasmadata = Plasma({
-      name: name,
+    const { firstName, lastName, email, password, phone, bloodGroup, address, city, pincode } = req.body.formData;
+    if(!firstName || !lastName || !email || !password || !phone || !address || !bloodGroup || !city || !pincode){
+      return res.status(401).json({
+        success: false,
+        message: "all fields are required!"
+      });
+    };
+
+    //checking if email is taken
+    const existingDonor = await Plasma.findOne({ email: email });
+    if (existingDonor) {
+      return res.status(400).json({
+          success: false,
+          message: "email already exists!"
+      });
+    };
+
+    const plasma = new Plasma({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashPassword(password),
       phone: phone,
       bloodGroup: bloodGroup,
-      cDate: cDate,
       address: address,
       city: city,
       pincode: pincode
     });
-    await plasmadata.save();
-    res.status(200).json(plasmadata);
-  } catch (e) {
-    console.log(`Error in creating a event: ${e}`);
-  }
+
+    plasma.save().then((plasma) => {
+      res.status(201).json({
+          success: true,
+          message: "donor saved successfully!",
+          plasma: plasma
+      });
+  }).catch(err => {
+      res.status(500).json({
+          success: false,
+          message: err.message
+      });
+  });
+});
+
+//*Route 3
+//Donor login
+
+router.post("/login/donor",async(req,res)=>{
+  const {email,password} = req.body.formData;
+  if(!email||!password){
+      return res.status(401).json({
+          success:false,
+          message:"all fields are required!"
+      });
+  };
+
+  const plasma =await Plasma.findOne({email:email});
+  if(!plasma){
+      return res.status(401).json({
+          success:false,
+          message:"incorrect username or password!"
+      });
+  };
+
+  const isMatch=comparePassword(password,plasma.password);
+  if(!isMatch){
+      return res.status(401).json({
+          success:false,
+          message:"incorrect username or password!"
+      });
+  };
+
+  res.status(200).json({
+      success:true,
+      message:"donor logged in successfully!",
+      plasma:plasma
+  });
 });
 
 module.exports = router;
