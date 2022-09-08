57f0675265630e927b4453f1d7e7931e479eea53
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-09-08 23:18:59 +0600

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import SignupForm from '../components/SignupForm'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import SignupForm from '../components/SignupForm';
 
 function Signup() {
   return (
-    <div style={{display:"grid",rowGap:"5vw"}}>
-      <Navbar/>
-      <SignupForm/>
-      <Footer/>
-    </div>
-  )
-}
+      <div className='form-container'>
+          <Navbar/>
+          <SignupForm/>
+          <Footer/>
+      </div>
+    );
+};
 
-export default Signup
+export default Signup;
