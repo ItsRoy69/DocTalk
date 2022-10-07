0dea7b1c21bbd7dad2780c65113d35fe13e7ba14
Ali Fahed <alifahed67@gmail.com>
2022-10-07 14:13:19 +0800

    Improve Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,62 +10,20 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-			<Link to="/" style={{ color: "inherit" }}>
-      <div className="navbar__slogan">
-        <div className="navbar__slogan__logo">
-          <img src={logo} alt="logo" />
+      <Link to="/" style={{ color: "inherit" }}>
+        <div className="navbar__slogan">
+          <div className="navbar__slogan__logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="navbar__slogan__name">DocTalk</div>
         </div>
-        <div className="navbar__slogan__name">DocTalk</div>
-      </div>
-			</Link>
+      </Link>
       <div className="navbar__links">
         <ul>
           <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <HashLink  smooth to="/#about">
-              About Us
+            <HashLink smooth to="/#home">
+              Home
             </HashLink>
-          </li>
-          <li>
-            <Link to="/plasmaDonor">Donors</Link>
-          </li>
-          <li>
-            <Link to="/doctors">Doctors</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li>
-            <HashLink smooth to="/#contact">
-              Contact
-            </HashLink>
-          </li>
-        </ul>
-      </div>
-      <div
-        className="navbar__hamburger-menu"
-        onClick={() => setShowMenu((value) => !value)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div
-        className={`navbar__mobile-side-nav ${
-          showMenu ? "navbar__mobile-side-nav--show" : ""
-        }`}
-      >
-        <ul>
-          <div
-            className="navbar__mobile-side-nav__exit"
-            onClick={() => setShowMenu(false)}
-          >
-            X
-          </div>
-          <li>
-            <Link to="/">Home</Link>
           </li>
           <li>
             <HashLink smooth to="/#about">
@@ -83,14 +41,76 @@ const Navbar = () => {
               Contact
             </HashLink>
           </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
         </ul>
+      </div>
+      <div
+        className="navbar__hamburger-menu"
+        onClick={() => setShowMenu((value) => !value)}
+      >
+        <div className="navbar__hamburger-menu-container">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div
+        className={`navbar__mobile-side-nav ${
+          showMenu ? "navbar__mobile-side-nav--show" : ""
+        }`}
+      >
+        <div
+          className="navbar__mobile-side-nav__exit"
+          onClick={() => setShowMenu(false)}
+        >
+          X
+        </div>
+        <div className="navbar__mobile-side-nav-container">
+          <ul>
+            <li>
+              <HashLink
+                smooth
+                to="/#home"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                Home
+              </HashLink>
+            </li>
+            <li>
+              <HashLink
+                smooth
+                to="/#about"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                About Us
+              </HashLink>
+            </li>
+            <li>
+              <Link to="/plasmaDonor">Donors</Link>
+            </li>
+            <li>
+              <Link to="/doctors">Doctors</Link>
+            </li>
+            <li>
+              <HashLink
+                smooth
+                to="/#contact"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                Contact
+              </HashLink>
+            </li>
+          </ul>
+        </div>
       </div>
       <div
         className={`navbar__shadow ${showMenu ? "navbar__shadow--show" : ""}`}
         onClick={() => setShowMenu(false)}
       ></div>
     </nav>
-	)
+  );
 };
 
 export default Navbar;
