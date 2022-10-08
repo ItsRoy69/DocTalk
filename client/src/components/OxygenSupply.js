afb9ee1fc8d9ffc76eedcc231e063b115731293b
Aarthi Suresh Kumar <aarthisureshkumar17@gmail.com>
2022-10-08 15:48:39 +0530

    Fix: Oxygen supplier page UI

--- client/src/components/OxygenSupply.js
+++ client/src/components/OxygenSupply.js
@@ -1,100 +1,127 @@
-// Oxygen Supplier Component 
+// Oxygen Supplier Component
 
 import {
-	BsFillPinMapFill as Pin,
-	BsStarFill as Star,
-	BsStarHalf as HalfStar,
-	BsFillTelephoneFill as Phone,
-	BsCashStack as Cash,
-	BsFillMapFill as Zone,
-	BsFillBellFill as Number,
-	BsBank as Hospital,
-} from "react-icons/bs"
+  BsFillPinMapFill as Pin,
+  BsStarFill as Star,
+  BsStarHalf as HalfStar,
+  BsFillTelephoneFill as Phone,
+  BsCashStack as Cash,
+  BsFillMapFill as Zone,
+  BsFillBellFill as Number,
+  BsBank as Hospital,
+} from "react-icons/bs";
 
-import "../styles/OxygenSupplier.css"
+import "../styles/OxygenSupplier.css";
 
 import { oxygenSupplierImage } from "../data/oxygenSuppliers.js";
 
 const Ratings = ({ rating }) => {
-	const full_stars = Math.floor(rating)
-	const half_rating = full_stars !== rating
-	return (
-		<div>
-			{[...Array(full_stars)].map((_, index) => (
-				<Star key={index} color="gold" size="1.5em" />
-			))}
-			{half_rating && <HalfStar color="gold" size="1.5em" />}
-		</div>
-	)
+  const full_stars = Math.floor(rating);
+  const half_rating = full_stars !== rating;
+  return (
+    <div>
+      {[...Array(full_stars)].map((_, index) => (
+        <Star key={index} color="gold" size="1.5em" />
+      ))}
+      {half_rating && <HalfStar color="gold" size="1.5em" />}
+    </div>
+  );
 };
 
 const OxygenSupplierCharge = ({ amount }) => {
-	const isFree = amount === "Free"
-	return (
-		<h5 style={{ color: isFree ? "#00ee00" : "white" }}>
-			Charges: {isFree ? amount : "₹"}
-			{!isFree && amount}
-		</h5>
-	)
+  const isFree = amount === "Free";
+  return (
+    <h5
+      style={{
+        color: isFree ? "#00ee00" : "white",
+        paddingLeft: 5,
+        paddingRight: 5,
+      }}
+    >
+      Charges: {isFree ? amount : "₹"}
+      {!isFree && amount}
+    </h5>
+  );
 };
 
 const OxygenSupply = ({ oxygenSupplier }) => {
-	const {
-		name,
-		phone,
-		hospital,
-		location: { address, city, country },
-		charges,
-		available,
-		rating,
-	} = oxygenSupplier;
+  const {
+    name,
+    phone,
+    hospital,
+    location: { address, city, country },
+    charges,
+    available,
+    rating,
+  } = oxygenSupplier;
 
-	return (
-		<div className="oxygenSupplier">
-			<div className="oxygenSupplier-container">
-				<h5 className="supplier-name">{name}</h5>
-				<div className="details">
-					<div className="info">
-						<div style={{ cursor: "pointer" }}>
-							<Phone size="1.2em" />
-							<h5 onClick={() => navigator.clipboard.writeText(phone)}>+91 {phone}</h5>
-						</div>
-						<div>
-							<Hospital size="1.5em" />
-							<h5 style={{ paddingRight: 5 }}>Hospital: </h5>
-							<h5> {hospital}</h5>
-						</div>
-						<div className="address">
-							<Pin size="1.5em" />
-							<h5>Address: </h5>
-							<h5> {address}</h5>
-						</div>
-						<div>
-							<Cash size="1.5em" />
-							<OxygenSupplierCharge amount={charges} />
-						</div>
-						<div>
-							<Number size="1.5em" />
-							<h5>
-								Available: {available} ambulance{available > 1 && "s"}
-							</h5>
-						</div>
-						<div>
-							<Zone size="1.5em" />
-							<h5>
-								Zone: {city}, {country}
-							</h5>
-						</div>
-						<div>
-							<h5 style={{ paddingRight: 10 }}>Rating:</h5>
-							<Ratings rating={rating} />
-						</div>
-					</div>
-					<img src={oxygenSupplierImage} alt="OxygenSupplierImage" />
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="oxygenSupplier">
+      <div className="oxygenSupplier-container">
+        <div className="d-flex bd-highlight">
+          <div className="p-4 flex-fill bd-highlight">
+            <h5 className="supplier-name">{name}</h5>
+          </div>
+          <div className="p-2 flex-fill bd-highlight">
+            <div className="details">
+              <div className="info">
+                <div style={{ cursor: "pointer" }}>
+                  <Phone size="2em" />
+                  <h5
+                    style={{ paddingLeft: 5, paddingRight: 5 }}
+                    onClick={() => navigator.clipboard.writeText(phone)}
+                  >
+                    +91 {phone}
+                  </h5>
+                </div>
+                <div>
+                  <Hospital size="2em" />
+                  <h5>Hospital: </h5>
+                  <h5> {hospital}</h5>
+                </div>
+                <div>
+                  <Pin size="2.5em" />
+                  <h5 style={{ paddingLeft: 7, paddingRight: 3 }}>Address: </h5>
+                  <h5> {address}</h5>
+                </div>
+                <div>
+                  <Cash size="2em" />
+                  <OxygenSupplierCharge amount={charges} />
+                </div>
+                <div>
+                  <Number size="2em" />
+                  <h5>
+                    Available: {available} ambulance{available > 1 && "s"}
+                  </h5>
+                </div>
+                <div>
+                  <Zone size="2em" />
+                  <h5>
+                    Zone: {city}, {country}
+                  </h5>
+                </div>
+                <div>
+                  <h5 style={{ paddingLeft: 25, paddingRight: 10 }}>Rating:</h5>
+                  <Ratings
+                    style={{ paddingLeft: 5, paddingRight: 5 }}
+                    rating={rating}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="oxygenSupplierImage flex-fill bd-highlight">
+            <img
+              src={oxygenSupplierImage}
+              width={"auto"}
+              height={"200px"}
+              alt="OxygenSupplierImage"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default OxygenSupply;
--- client/src/pages/OxygenSupplier.js
+++ client/src/pages/OxygenSupplier.js
@@ -1,72 +1,100 @@
-import React from 'react';
+import React from "react";
 import { useState, useEffect } from "react";
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 import "../styles/OxygenSupplier.css";
 
-
 import OxygenSupplier from "../components/OxygenSupply.js";
-import {oxygenSuppliers} from "../data/oxygenSuppliers.js";
+import { oxygenSuppliers } from "../data/oxygenSuppliers.js";
 // import { banner } from "../data/oxygenSupplier";
 
 import { BsSearch as Search } from "react-icons/bs";
 import CreatableSelect from "react-select/creatable";
 
 const getDistinctLocations = (data) => {
-  const allZones = [].concat(data.map(oxygenSupplier => oxygenSupplier.location.city));
-  const distinctSet = new Set(allZones);
-  return [...distinctSet].map(zone => ({ value: zone, label: zone }));
+  const allZones = [].concat(
+    data.map((oxygenSupplier) => oxygenSupplier.location.city)
+  );
+  let distinctSet = new Set(allZones);
+  return [...distinctSet].map((zone) => ({ value: zone, label: zone }));
 };
 
 const OxygenSupplierPage = () => {
   document.title = "Oxygen Supplier | DocTalk";
-  const [location, setLocation] = useState(null);
+
+  const [location, setLocation] = useState("");
   const [loading, setLoading] = useState(false); // Later controlled by states when fetching from database
   const Zones = getDistinctLocations(oxygenSuppliers);
-  const locatedOxygenSuppliers = location
-    ? oxygenSuppliers.filter((oxygenSupplier) => oxygenSupplier.location.city === location)
-    : oxygenSuppliers;
-  const handleSelect = (e) => {
-    setLoading(true);
-    setLocation(e?.value);
-  };
+  const [locatedOxygenSuppliers, setLocatedOxygenSuppliers] = useState([]);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2000); // Loading time to mimic database fetching time
-  }, [loading]);
+    onChangeLocatedOxygenSuppliers();
+  }, [location]);
+
+  const onChangeLocatedOxygenSuppliers = () => {
+    setLoading(true); // start fetching from DB
+    setLocatedOxygenSuppliers(
+      location
+        ? oxygenSuppliers.filter(
+            (oxygenSupplier) => oxygenSupplier.location.city === location
+          )
+        : []
+    );
+    setLoading(false); // completed fetch from DB
+  };
+
+  const handleSelect = (e) => {
+    if (!e) {
+      setLocation("");
+    } else {
+      setLocation(e.value);
+    }
+  };
+
+  // useEffect(() => {
+  //   setTimeout(() => setLoading(false), 2000); // Loading time to mimic database fetching time
+  // }, [loading]);
 
   return (
     <>
       <Navbar />
 
-      <div className="container-fluid head-container">
-        <h3 className="main-heading">Available Oxygen Suppliers</h3>
-        <div className="searchBar">
-          <Search size={40} color="#0603E6" />
-          <div style={{ maxWidth: 450, width: "100%" }}>
-            <CreatableSelect
-              placeholder="Select Region"
-              isClearable
-              onChange={handleSelect}
-              options={Zones}
-              menuPortalTarget={document.querySelector("body")}
-            />
+      <div className="head-container">
+        <div className="search-container">
+          <div className="p-2 bd-highlight">
+            <h3 className="oxygenSupplierHeading">
+              Available Oxygen Suppliers
+            </h3>
+          </div>
+          <div className="p-2 bd-highlight">
+            <div className="searchBar">
+              <Search className="searchIcon" size="35" color="#0603E6" />
+              <CreatableSelect
+                className="oxygenSupplierSelectBox"
+                onChange={handleSelect}
+                placeholder="Select a location"
+                options={Zones}
+                // menuPortalTarget={document.querySelector("body")}
+                isClearable
+              />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="container loading-container">
+      <div className="loading-container">
         {loading ? (
           <h5 style={{ textAlign: "center" }}>Loading...</h5>
         ) : locatedOxygenSuppliers.length ? (
-          locatedOxygenSuppliers.map(item => 
+          locatedOxygenSuppliers.map((item) => (
             <OxygenSupplier oxygenSupplier={item} key={item._id} />
-          )
+          ))
         ) : (
           <h5 style={{ textAlign: "center" }}>
-            Currently no Oxygen Suppliers or Services were found in the selected region. 
+            Currently no Oxygen Suppliers or Services were found in the selected
+            region.
           </h5>
         )}
       </div>
