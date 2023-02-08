08d632dbfba166da6385e3da5a309b4eb21c0146
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-02-08 10:17:33 +0530

    fix: On Mobile view header section text are to close, Do not reload after clicking the contact button. New Feature: Adding Lazy loading feature.

---
+++ client/src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loader() {
+    return (
+        <div style={{ display: "flex", justifyContent: "center", paddingTop: "4vh", fontFamily: "serif" }}>
+            <p>Loading...</p>
+        </div>
+    )
+}
+
+export default Loader;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,94 +1,96 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import headingPhoto from "../assets/images/heading.png";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
-import "../styles/Home.css";
-import Services from "../components/Services";
-import HelpingHand from "../components/HelpingHand";
+import { useEffect, useState } from 'react';
 import ContactForm from "../components/ContactForm";
-import {useState , useEffect} from 'react'
+import HelpingHand from "../components/HelpingHand";
+import Services from "../components/Services";
+import "../styles/Home.css";
 
-function Home(){
+function Home() {
 
-    const [formData, setFormData] = useState();
-    const [firstName, setFirstName] = useState();
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [subject, setSubject] = useState();
-    const [message, setMessage] = useState();
+  const [formData, setFormData] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [subject, setSubject] = useState();
+  const [message, setMessage] = useState();
 
-  function handleSubmit(){
+  function handleSubmit(e) {
+    e.preventDefault();
+    
     const request = {
-        method:'POST',
-        mode:'cors',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        redirect:'follow',
-        referrerPolicy:'no-referrer',
-        body: JSON.stringify({formData})
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify({ formData })
     };
 
-    const myRequest = new Request("http://localhost:5000/contact",request);
+    const myRequest = new Request("http://localhost:5000/contact", request);
     fetch(myRequest).then(function (response) {
-        return response;
+      return response;
     }).then(function (response) {
-        console.log(response);
+      console.log(response);
     }).catch(function (e) {
-        console.log(e);
+      console.log(e);
     });
-}
+  }
 
 
-useEffect(()=>{
-setFormData({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    subject: subject,
-    message: message
-});
-},[firstName, lastName, email, subject, message]);
-  return(
+  useEffect(() => {
+    setFormData({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      subject: subject,
+      message: message
+    });
+  }, [firstName, lastName, email, subject, message]);
+  return (
     <>
-    <Navbar />
-    <div className="container">
-      <div className="heading" id="home">
-        <div className="heading__info">
-          <div className="heading__info__title">DocTalk</div>
-          <div className="heading__info__subtitle">
-            MedHelp just a click away...
+      <Navbar />
+      <div className="container">
+        <div className="heading" id="home">
+          <div className="heading__info">
+            <div className="heading__info__title">DocTalk</div>
+            <div className="heading__info__subtitle">
+              MedHelp just a click away...
+            </div>
+            <button className="heading__info__cta">Get In Touch</button>
           </div>
-          <button className="heading__info__cta">Get In Touch</button>
+          <div className="heading__lcp">
+            <img src={headingPhoto} alt="heading" width="600" height="600" />
+          </div>
         </div>
-        <div className="heading__lcp">
-          <img src={headingPhoto} alt="heading" width="600" height="600" />
+        <div className="aboutus" id="about">
+          <h4>About Us</h4>
+          <div className="aboutus-container">
+            <p className="aboutus-text">
+              DocTalk is your ultimate medical help space. Here, at DocTalk you
+              will be helped with all the services catering towards the medical
+              field like Plasma donors' hospital beds oxygen suppliers medicine
+              suppliers and ambulance providers. DocTalk is your one step
+              solution. Just log into your account and book your appointment right
+              away. you can even call us for your appointment, and we will get
+              back to you shortly.
+            </p>
+          </div>
         </div>
+
+        <Services />
+        <HelpingHand />
+        <ContactForm />
+
+
       </div>
-      <div className="aboutus" id="about">
-        <h4>About Us</h4>
-        <div className="aboutus-container">
-          <p className="aboutus-text">
-            DocTalk is your ultimate medical help space. Here, at DocTalk you
-            will be helped with all the services catering towards the medical
-            field like Plasma donors' hospital beds oxygen suppliers medicine
-            suppliers and ambulance providers. DocTalk is your one step
-            solution. Just log into your account and book your appointment right
-            away. you can even call us for your appointment, and we will get
-            back to you shortly.
-          </p>
-        </div>
-      </div>
-
-      <Services />
-      <HelpingHand />
-      <ContactForm />
-
-
-    </div>
-    <Footer />
-  </>
+      <Footer />
+    </>
   )
 }
 
